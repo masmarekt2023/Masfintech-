75a0ee3fc72b55552a17dc9ece81e3a8f6da372d
judi-sde <software.engineering.official@gmail.com>
2025-01-31 17:17:00 +0200

    Delete app directory

--- app/ui/fonts.ts
+++
@@ -1,4 +0,0 @@
-import { Saira, Noto_Kufi_Arabic } from "next/font/google";
-
-export const saira = Saira({ subsets: ["latin"], weight: "400" });
-export const kufi = Noto_Kufi_Arabic({ subsets: ["latin"], weight: "400" });
